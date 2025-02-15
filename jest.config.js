e4b30e0ee220899df427e5b0398597ad79435e57
Wilson Gonçalves <wilssp@gmail.com>
2025-02-15 11:33:14 -0300

    Revision Application Configuration

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1"
+  },
+};
