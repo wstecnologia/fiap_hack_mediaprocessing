c9b4798b44a1fae695eef369d22bf3a40b127b30
Wilson Gonçalves <wilssp@gmail.com>
2025-02-18 19:51:21 -0300

    Docker compose config

--- dockerfile
+++ dockerfile
@@ -2,12 +2,16 @@ FROM node:18-alpine
 
 WORKDIR /app
 
-COPY . .
+RUN apk add --no-cache bash curl
+
+COPY package.json package-lock.json ./
 
 RUN npm install
+
+COPY . .
 
 RUN npm run build
 
 EXPOSE 3000
 
-CMD ["npm","start"]
+CMD ["npm", "start"]
--- src/infrastructure/repositories/RedisRepository.ts
+++ src/infrastructure/repositories/RedisRepository.ts
@@ -7,6 +7,7 @@ class RedisRepository {
     this.redis = new Redis({
       host: process.env.REDIS_HOST || 'localhost', 
       port: Number(process.env.REDIS_PORT) || 6379,
+       password: process.env.REDIS_PASSWORD, 
       socketTimeout: 1000, 
     });
 
